cd7e8d72cd398460e71baa83270b731a412ef98e
Miri Michaelson <miriammichaelson@gmail.com>
2020-07-13 16:39:09 +0100

    Linked up controller (app.js) to login.ejs

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('login'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
